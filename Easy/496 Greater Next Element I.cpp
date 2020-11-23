8415d11e09a60c72c5067f5a30d30f1023ec6064
Anmol Kaur <anmolkaurminhas@gmail.com>
2020-11-23 19:36:43 +0530

    Create 496 Greater Next Element I.cpp

---
+++ Easy/496 Greater Next Element I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        unordered_map<int,int> m;
+        for(int n: nums2){
+            while(s.size() && s.top()<n ){
+                m[s.top()] = n;
+                s.pop();
+            }
+            s.push(n);
+        }
+        vector<int> result;
+        for(int n: nums1){
+            result.push_back(m.count(n)? m[n]: -1);
+        }
+        return result;
+    }
+};
