d8f652526b4b0c316ec0baa59654b2be1b64d8a2
Anmol Kaur <anmolkaurminhas@gmail.com>
2020-11-26 13:24:09 +0530

    Create 933. Number of Recent Calls.cpp

---
+++ Easy/933. Number of Recent Calls.cpp
@@ -0,0 +1,21 @@
+class RecentCounter {
+public:
+    queue<int> q;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+        while(!q.empty() && q.front() < t- 3000){ //At time t only [(t-3000)->t] will remain in the queue
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter* obj = new RecentCounter();
+ * int param_1 = obj->ping(t);
+ */
